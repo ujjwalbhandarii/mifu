4de8a9ce9dc86b49804fb15d916abf1a71f4abd5
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-06-17 23:04:33 +0545

    updated: container width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
         </figure>
       </nav>
       <section className="flex items-center justify-center gap-3 flex-col text-center my-4 md:my-10 lg:max-w-[70%] mx-auto">
-        <h1 className="text-2xl font-extrabold leading-tight tracking-wide md:mb-3">
-          Become an Influencer For <br /> Breakaway x Mifu
+        <h1 className="text-2xl font-extrabold leading-tight tracking-wide md:mb-3 md:w-[40rem]">
+          Become an Influencer For Breakaway x Mifu
         </h1>
         <p className="text-base font-medium">
           Whether you&apos;re the person with the most likes or followers on
